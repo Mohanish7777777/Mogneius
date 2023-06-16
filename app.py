be351e9ec4d035d7227789ee1e3c8945eed0d07d
Mohanish <111619160+Mohanish7777777@users.noreply.github.com>
2023-06-16 18:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://Mohanish7777777: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@github.com/Mohanish7777777/LazyPrincessV2 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
